e07b717471b60546395633a0141abf76db1d8d82
Robinson <robincoello@hotmail.com>
2017-07-23 22:53:50 +0200

    inicial

---
+++ _grupos/tr.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+    echo ' <tr>
+    <td>'.$i.'</td> 
+ <td>'.$_grupos_grupo.'</td> 
+ <td>'._usuarios_total_segun_grupo($_grupos_grupo).'</td> 
+ <td>
+<a href='.$_SERVER['PHP_SELF'].'?p=_grupos&c=ver&_grupos_id='.$_grupos_id.'>Ver</a> |  
+<a href='.$_SERVER['PHP_SELF'].'?p=_grupos&c=editar&_grupos_id='.$_grupos_id.'>Editar</a>  
+                      
+                </td></tr>';  
